78f8620a455a34b227c0f3ca2503ef899b9cff4f
Ravikant Tyagi <dhruvasharma1989@gmail.com>
2023-03-25 13:52:43 +0530

    Add files via upload

---
+++ Javascripts/AdvCalc/script.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", init);
+
+function init() {
+    var textBox = document.querySelector("input");
+    textBox.setAttribute("readonly", true);
+}
--- Javascripts/BasicCalculator/script_3.js
+++ Javascripts/BasicCalculator/script_3.js
@@ -4,10 +4,18 @@ function initEvents() {
     fnum = document.querySelector("#box_1");
     snum = document.querySelector("#box_2");
     // document.getElementById("add").addEventListener("click", add);
-    document.querySelector("#add").addEventListener("click", calc);
-    document.querySelector("#sub").addEventListener("click", calc);
-    document.querySelector("#div").addEventListener("click", calc);
-    document.querySelector("#mul").addEventListener("click", calc);
+    // document.querySelector("#add").addEventListener("click", calc);
+    // document.querySelector("#sub").addEventListener("click", calc);
+    // document.querySelector("#div").addEventListener("click", calc);
+    // document.querySelector("#mul").addEventListener("click", calc);
+    var btns = document.querySelectorAll("button");
+    // for(var i = 0; i < btns.length; i++) {
+    //     btns[i].addEventListener("click", calc);
+    // }
+
+    for (var btn of btns) {
+        btn.addEventListener("click", calc);
+    }
 }
 
 function calc() {
