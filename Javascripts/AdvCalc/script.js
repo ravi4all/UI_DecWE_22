ba0ba92fc501076d6c5735be78c16a07d359cc48
Ravikant Tyagi <dhruvasharma1989@gmail.com>
2023-03-25 14:32:07 +0530

    Add files via upload

--- Javascripts/AdvCalc/script.js
+++ Javascripts/AdvCalc/script.js
@@ -1,6 +1,26 @@
 window.addEventListener("load", init);
 
+var textBox;
+var boxValue;
 function init() {
-    var textBox = document.querySelector("input");
+    textBox = document.querySelector("input");
     textBox.setAttribute("readonly", true);
+    var btns = document.querySelectorAll(".num");
+    for (var btn of btns) {
+        btn.addEventListener("click", appendNum);
+    }
+
+    var oprs = document.querySelectorAll(".opr");
+    for (var opr of oprs) {
+        opr.addEventListener("click", appendOperator);
+    }
+}
+
+function appendNum() {
+    textBox.value += this.innerHTML;
+    boxValue = textBox.value;
+}
+
+function appendOperator() {
+    textBox.value = boxValue + this.innerHTML;
 }
